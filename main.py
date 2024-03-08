f45e764c550a7d25813e6285c5c3637f91655433
Arsene2005 <arayimbengue9@gmail.com>
2024-03-08 20:54:30 +0000

    Added code to display user's birth year

--- main.py
+++ main.py
@@ -4,3 +4,9 @@ print("Bonjour,", first_name, last_name)
 
 age = input("Entrez votre âge : ")
 print("Vous avez", age, "ans.")
+
+rom datetime import datetime
+
+current_year = datetime.now().year
+birth_year = current_year - int(age)
+print("Vous êtes né(e) en", birth_year)
